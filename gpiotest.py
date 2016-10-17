b930253f7f7fb074b5fe4810767e1ed71d278247
Adam Casey <adamjcasey@gmail.com>
2016-10-17 01:05:41 +0000

    Fixed gpiodemo

--- gpiotest.py
+++ gpiotest.py
@@ -3,9 +3,9 @@ import base64
 import hashlib
 
 
-MICA = "mica-efjyn.local"
+MICA = "192.168.137.100"
 
-GPIO_CONTAINER = "GPIOTest"
+GPIO_CONTAINER = "GPIODemo"
 ROLE = "admin"
 PW = "admin"
 
@@ -19,17 +19,19 @@ def getCredentials(role, passwd):
 
 cred = getCredentials(ROLE,PW)
 
-payload = {"jsonrpc":"2.0","id":1,"method":"get_auth_token","params": [cred]}
+payload = {"jsonrpc":"2.0","id":1,"method":"get_auth_token","params": ["admin", base64.b64encode(PW.encode())]}
 
 
 payload_as_string = json.dumps(payload)
+print(payload_as_string)
 
 http = urllib3.PoolManager( assert_hostname=False, ca_certs="/META/harting_web.crt")
 
 at = http.urlopen("POST", "https://" + MICA + "/base_service/", body=payload_as_string)
 
-rep_data_str = str(at.data, "utf-8")
+rep_data_str = str(at.data)
 
+print(rep_data_str)
 ret = json.loads(rep_data_str)
 
 retl = ret["result"][1]
@@ -38,8 +40,8 @@ ws_url = "wss://"+MICA+"/"+GPIO_CONTAINER+"/"
 
 wesckt = websocket.create_connection(ws_url,sslopt = {"cert_reqs":ssl.CERT_NONE, "ca_certs":"/META/harting.crt", "check_hostame":False})
 
-call = {"id":1, "method":"login", "params": [ret1]}
-weskt.send(json.dumps(call))
+call = {"id":1, "method":"login", "params": [retl]}
+wesckt.send(json.dumps(call))
 
 print(json.dumps(call))
 
@@ -47,7 +49,7 @@ result_str = wesckt.recv()
 
 print(result_str)
 
-call = {"id":1,"method":"get_pin_states", "params":[0]}
+call = {"id":1,"method":"set_state","params":[0,0]}
 
 wesckt.send(json.dumps(call))
 
