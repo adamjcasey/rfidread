799e06167f7ad49b414d3b7b6b8b88028a34fa4b
Adam Casey <acasey@continuuminnovation.com>
2017-05-19 11:20:29 -0400

    Updated demo to read from database

--- demo.py
+++ demo.py
@@ -1,6 +1,5 @@
 import struct
-import time
-import sys
+import MySQLdb
 
 infile_path = "/dev/input/event0"
 FORMAT = 'llHHI'
@@ -14,17 +13,33 @@ event = in_file.read(EVENT_SIZE)
 out_str = ''
 
 def rfid_card_read(id):
-	print ("RFID: %s" % (id))
+    print ("RFID: %s" % (id))
 
+    # Open database connection and get the approval
+    db = MySQLdb.connect("192.168.137.1", "root", "root", "rfiddemo")
+    cursor = db.cursor()
+    sql = "SELECT toolaccess FROM user WHERE tagvalue=%s" % (id)
+    try:
+        cursor.execute(sql)
+        results = cursor.fetchone()
+        print "Access = %d" % (results[0])
+    except:
+        print "Error: unable to fecth data"
+
+    db.close()
+
+
+###------------------------------------------------------------------------ 
 while event:
-	(tv_sec,tv_usec, type, code, value) = struct.unpack(FORMAT, event)
-	if type == 1 and value == 1:
-		if code <= 11:
-			out_str += str(code_to_ascii[code])
-		else:
-			rfid_card_read(out_str)
-			out_str = ''
-	
-	event = in_file.read(EVENT_SIZE)
+    (tv_sec,tv_usec, type, code, value) = struct.unpack(FORMAT, event)
+    if type == 1 and value == 1:
+        if code <= 11:
+            out_str += str(code_to_ascii[code])
+        else:
+            rfid_card_read(out_str)
+            out_str = ''
+    
+    event = in_file.read(EVENT_SIZE)
 
 in_file.close()
+
